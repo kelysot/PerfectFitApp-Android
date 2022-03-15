6ce1f00e11c9bded7662e5774e0e374fd27466b6
kelysot <kelysot@gmail.com>
2022-03-15 19:13:04 +0200

    Add Bottom Navigation View that have Home and Profile, in the future will be add Notification and Wishlist.

--- app/src/main/java/com/example/perfectfitapp_android/MainActivity.java
+++ app/src/main/java/com/example/perfectfitapp_android/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.perfectfitapp_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.NavHost;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 import android.view.Menu;
+
+import com.example.perfectfitapp_android.profile.ProfileFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,11 +27,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+
         mAppBarConfiguration = new AppBarConfiguration.Builder().build();
 
         NavHost navHost = (NavHost) getSupportFragmentManager().findFragmentById(R.id.main_navhost);
         navCtl = navHost.getNavController();
 
+        NavigationUI.setupWithNavController(bottomNavigationView, navCtl);
         NavigationUI.setupActionBarWithNavController(this, navCtl);
 
     }
@@ -39,5 +49,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.main_navhost);
         return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_navhost, fragment);
+        fragmentTransaction.commit();
+
     }
 }
